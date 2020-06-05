7ae7fd45eae1f5eb57540f14566b16f0dd8a55f2
Juliana <juliana.mg3@gmail.com>
2020-06-05 01:45:53 -0300

    ejercicios de funciones

---
+++ desafio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 20;
+	int b = 23;
+	int c = a + b;
+	int d = a - b;
+	int e = a * b;
+	int f = a / b;
+
+
+	printf("La suma de a + b es %d \n", c);
+	printf("La resta de a - b es %d \n", d);
+	printf("El producto de a * b es %d \n", e);
+	printf("El cociente de la divicion de a entre b es %d \n", f);
+
+}
+
---
+++ fuction.c
@@ -0,0 +1,12 @@
+int f( int a )
+
+{
+	return a * 2;
+}
+
+
+
+int main ()
+{
+	return f(5);
+}
